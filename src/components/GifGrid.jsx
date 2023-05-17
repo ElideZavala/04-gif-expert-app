3039c655450c697c90cfd7c670aa86b7e916bc19
Elide <zavalavinagreelide@gmail.com>
2023-05-17 15:55:07 -0600

    useEffect new

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,22 @@
 import { PropTypes } from "prop-types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-    const getGifs = [
-        {
-            id: "ABC",
-            title: "Cualquier cosa",
-        },
-        {
-            id: "DEF",
-            title: "Cualquier cosa 2",
-        },
-    ];
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
-        return () => {};
-    }, []);
+        getGifs(category).then(setImages);
+    }, [category]);
 
     return (
         <>
             <h3>{category}</h3>
-            {getGifs.map((img) => (
-                <li key={img.id}>{img.title}</li>
+            {images.map(({ id, url, title }) => (
+                <div key={id}>
+                    <li>{title}</li>
+                    <img src={url} alt={title} />
+                </div>
             ))}
         </>
     );
